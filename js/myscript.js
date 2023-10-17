d25fce989f559ec204c6f9fa3e6bb92541cffbdd
willian <34694258+willian-hm@users.noreply.github.com>
2023-10-17 11:28:09 +0000

    a

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+alert("olá galera!")
+
+function mudarFrase() {
+    var elemento = document.getElementById("frase")
+    elemento.innerHTML = "Qualquer frase"
+    elemento.style.color = "red";
+    elemento.style.fontSize = "25px";
+    elemento.style.backgroundColor = "yellow";
+    document.body.style.backgroundColor = "blue";
+    elemento.innerHTML = "<img src=\'img/a.jpeg\' />"
+}
